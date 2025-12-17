c585213cf76b556271a22de381f4ef5cf1e14020
willie6666 <89621369+willie6666@users.noreply.github.com>
2025-12-17 09:47:15 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default {
 	async fetch(request): Promise<Response> {
-		const destinationURL = "https://example.com";
+		const destinationURL = "https://play.workadventu.re/_/ddowyftas1/hackersir.org/2023-workadventure-map/map.tmj";
 		const statusCode = 301;
 		return Response.redirect(destinationURL, statusCode);
 	},
